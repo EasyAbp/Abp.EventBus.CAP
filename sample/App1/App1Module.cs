ac77c014ed1e2d816d79daba6d61482f17d8d086
geffzhang <geffzhang@qq.com>
2021-06-08 10:25:55 +0800

    feat : kafka 队列测试 300k

--- sample/App1/App1Module.cs
+++ sample/App1/App1Module.cs
@@ -31,7 +31,7 @@ namespace App1
             context.AddCapEventBus(capOptions =>
             {
                 capOptions.UseInMemoryStorage();
-                capOptions.UseRabbitMQ("localhost");//UseRabbitMQ 服务器地址配置，支持配置IP地址和密码
+                capOptions.UseKafka("localhost"); 
                 capOptions.UseDashboard();//CAP2.X版本以后官方提供了Dashboard页面访问。
             });
             ConfigureSwaggerServices(context, configuration);
--- sample/App2/App2Module.cs
+++ sample/App2/App2Module.cs
@@ -46,7 +46,7 @@ namespace App2
                 // Options, Notice: You don't need to config x.UseSqlServer(""") again! CAP can autodiscovery.
                 //capOptions.UseEntityFramework<AppDbContext>();
                 capOptions.UseInMemoryStorage();
-                capOptions.UseRabbitMQ("localhost");//UseRabbitMQ 服务器地址配置，支持配置IP地址和密码
+                capOptions.UseKafka("localhost"); 
                 capOptions.UseDashboard();//CAP2.X版本以后官方提供了Dashboard页面访问。
             });
 
