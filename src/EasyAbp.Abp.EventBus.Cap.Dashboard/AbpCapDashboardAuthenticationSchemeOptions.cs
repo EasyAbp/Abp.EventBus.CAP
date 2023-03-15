1f658c58292feffe58f5974b50bdd51bd69fd832
gdlcf88 <gdlcf88@outlook.com>
2023-03-15 23:13:55 +0800

    Remove Dashboard

--- src/EasyAbp.Abp.EventBus.Cap.Dashboard/AbpCapDashboardAuthenticationSchemeOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Authentication;
-
-namespace EasyAbp.Abp.EventBus.CAP;
-
-public class AbpCapDashboardAuthenticationSchemeOptions : AuthenticationSchemeOptions
-{
-    public string PermissionName { get; set; }
-}
