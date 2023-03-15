e43740d88fde715f33bcf5a3a6081ded0ba02577
gdlcf88 <gdlcf88@outlook.com>
2023-03-15 23:14:21 +0800

    Add Dashboard

---
+++ src/EasyAbp.Abp.EventBus.CAP.Dashboard/AbpEventBusCapDashboardModule.cs
@@ -0,0 +1,37 @@
+﻿using EasyAbp.Abp.EventBus.CAP;
+using EasyAbp.Abp.EventBus.CAP.Localization;
+using EasyAbp.Abp.EventBus.CAP.Permissions;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Authorization;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace EasyAbp.Abp.EventBus.Cap
+{
+    [DependsOn(typeof(AbpAuthorizationModule))]
+    public class AbpEventBusCapDashboardModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAuthentication()
+                .AddScheme<AbpCapDashboardAuthenticationSchemeOptions, AbpCapDashboardAuthenticationHandler>(
+                    AbpCapDashboardAuthenticationHandler.SchemeName,
+                    options => { options.PermissionName = CapDashboardPermissions.Manage; });
+            
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<AbpEventBusCapDashboardModule>();
+            });
+
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Resources
+                    .Add<CapDashboardResource>("en")
+                    .AddVirtualJson("/Localization/CapDashboard");
+
+                options.DefaultResourceType = typeof(CapDashboardResource);
+            });
+        }
+    }
+}
